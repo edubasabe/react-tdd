73b153001eefe03a99cb6444bcd2c8177f30b641
David Basabe <david.basabe@skyairline.com>
2020-07-09 00:32:46 -0400

    :white_check_mark: Third test of counter and global setting of wrapper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <h1>This is counter app</h1>
+      <div id="counter-value">{counter}</div>
       <button id="increment-button">Increment</button>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -8,14 +8,21 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 describe('Counter testing', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+  
   test('render the title of counter', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('h1').text()).toContain("This is counter app");
   });
 
   test('render a button with the text of `increment`', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('#increment-button').text()).toBe("Increment");
+  });
+
+  test('render the initial value of state in a div', () => {
+    expect(wrapper.find('#counter-value').text()).toBe("0");
   });
 });
 
